f1daf045b736fd6265235553633e81385bc162e7
openi40 <architectures@openi40.org>
2022-07-29 11:03:10 +0200

    Implemented basic unit tests on IOMessages standard flow, TODO: implement acquired state changes in active scheduling algorithm.

--- openi40.scheduler/Engine.Standalone/src/test/java/com/openi40/scheduler/tests/IOMessagesTests.java
+++ openi40.scheduler/Engine.Standalone/src/test/java/com/openi40/scheduler/tests/IOMessagesTests.java
@@ -22,13 +22,23 @@ import com.openi40.scheduler.engine.aps.IApsMessagesBroker.ApsMessageResponse;
 import com.openi40.scheduler.engine.contextualplugarch.IContextualBusinessLogicFactory;
 import com.openi40.scheduler.engine.messages.handling.MessageHandlingErrorMessage;
 import com.openi40.scheduler.engine.standalone.Main;
+import com.openi40.scheduler.model.ReservableObjectAvailability;
 import com.openi40.scheduler.model.aps.ApsData;
 import com.openi40.scheduler.model.aps.ApsLogicOptions;
 import com.openi40.scheduler.model.aps.ApsSchedulingSet;
 import com.openi40.scheduler.model.dao.DataModelDaoException;
+import com.openi40.scheduler.model.dao.IMachineDao;
+import com.openi40.scheduler.model.dao.ITaskDao;
 import com.openi40.scheduler.model.dao.IWorkOrderDao;
+import com.openi40.scheduler.model.equipment.Machine;
+import com.openi40.scheduler.model.messages.EndSetupMessage;
+import com.openi40.scheduler.model.messages.EndWorkMessage;
 import com.openi40.scheduler.model.messages.StartSetupMessage;
+import com.openi40.scheduler.model.messages.StartWorkMessage;
+import com.openi40.scheduler.model.messages.TaskProductionUpdateMessage;
 import com.openi40.scheduler.model.orders.WorkOrder;
+import com.openi40.scheduler.model.tasks.Task;
+import com.openi40.scheduler.model.tasks.TaskStatus;
 
 import junit.framework.Assert;
 
@@ -45,6 +55,10 @@ public class IOMessagesTests {
 	TestsApsDataSourceUncachedFactory uncachedAccessor;
 	@Autowired
 	IWorkOrderDao workOrderDao;
+	@Autowired
+	ITaskDao taskDao;
+	@Autowired
+	IMachineDao machineDao;
 	static final String dataSourceName = "SS-COMPANY-DEMO";
 	static final String dataSetName = "STAINLESS-STEEL-COMPANY";
 	static final String dataSetVariant = "DEFAULT";
@@ -74,18 +88,88 @@ public class IOMessagesTests {
 		Assert.assertTrue("Message correctly handled=>false", response.isMessageHandled());
 	}
 
+	private void checkMachineState(String machineCode, ApsData data, ReservableObjectAvailability state)
+			throws DataModelDaoException {
+		Machine machine = machineDao.findByCode(machineCode, data);
+		Assert.assertEquals(
+				"Required machine state=>" + state.name() + " but is => "
+						+ (machine.getAvailability() != null ? machine.getAvailability().name() : "null"),
+				machine.getAvailability(), state);
+	}
+
+	private void checkTaskState(String taskCode, ApsData data, TaskStatus state) throws DataModelDaoException {
+		Task task = taskDao.findByCode(taskCode, data);
+		Assert.assertEquals("Required task state=>" + state.name() + " but is => "
+				+ (task.getStatus() != null ? task.getStatus().name() : "null"), task.getStatus(), state);
+	}
+
+	private void checkChoosedMachinCoherency(String taskCode, String machineCode) {
+		// TODO: IMPLEMENT THIS METHOD ONCE SCHEDULING ACTS MESSAGES COMMANDS COHERENTLY
+	}
+
 	@Test
-	public void correctMessagesFlowTest() throws ApsDataCacheException {
+	public void correctMessagesFlowTest() throws ApsDataCacheException, DataModelDaoException {
+
 		ApsData apsData = uncachedAccessor.loadData(dataSourceName, dataSetName, dataSetVariant);
 		setScheduleAllWorkOrders(apsData, ApsLogics.FORWARD_APS);
 		IApsLogicComposer composer = ComponentFactory.create(IApsLogicComposer.class, apsData, apsData);
 		composer.schedule(apsData, null);
+		long timestamp = System.currentTimeMillis();
+		String machineCode = "SS-LASERCUTMACHINE-02";
+		String taskCode = "SS/ORD003-2020/001-AISI316SQUARE50x50x3mm-1-001";
+		// Sending startSetupMessage and verifiyng status coherences
 		IApsMessagesBroker broker = ComponentFactory.create(IApsMessagesBroker.class, apsData, apsData);
 		StartSetupMessage startSetupMessage = new StartSetupMessage(apsData);
-		startSetupMessage.setMessageTimestamp(Timestamp.valueOf("2021-04-09 08:59:59"));
-		startSetupMessage.setTaskCode("SS/ORD003-2020/001-AISI316SQUARE50x50x3mm-1-001");
-		startSetupMessage.setMachineCode("SS-LASERCUTMACHINE-01");
+		startSetupMessage.setMessageTimestamp(Timestamp.valueOf("2021-01-09 08:59:59"));
+		startSetupMessage.setTaskCode(taskCode);
+		startSetupMessage.setMachineCode(machineCode);
 		ApsMessageResponse response = broker.handleMessage(startSetupMessage, apsData, null);
 		checkApsMessageResponse(response);
+		checkMachineState(machineCode, apsData, ReservableObjectAvailability.EXECUTING_SETUP);
+		checkTaskState(taskCode, apsData, TaskStatus.EXECUTING_SETUP);
+		checkChoosedMachinCoherency(taskCode, machineCode);
+
+		EndSetupMessage endSetupMessage = new EndSetupMessage(apsData);
+		endSetupMessage.setMessageTimestamp(Timestamp.valueOf("2021-01-09 09:05:59"));
+		endSetupMessage.setTaskCode(taskCode);
+		endSetupMessage.setMachineCode(machineCode);
+		response = broker.handleMessage(endSetupMessage, apsData, null);
+		checkApsMessageResponse(response);
+		checkMachineState(machineCode, apsData, ReservableObjectAvailability.SETUP_DONE);
+		checkTaskState(taskCode, apsData, TaskStatus.SETUP_DONE);
+		checkChoosedMachinCoherency(taskCode, machineCode);
+
+		StartWorkMessage startWorkMessage = new StartWorkMessage(apsData);
+		startWorkMessage.setMessageTimestamp(Timestamp.valueOf("2021-01-09 09:06:59"));
+		startWorkMessage.setTaskCode(taskCode);
+		startWorkMessage.setMachineCode(machineCode);
+		response = broker.handleMessage(startWorkMessage, apsData, null);
+		checkApsMessageResponse(response);
+		checkMachineState(machineCode, apsData, ReservableObjectAvailability.EXECUTING_WORK);
+		checkTaskState(taskCode, apsData, TaskStatus.EXECUTING_WORK);
+		checkChoosedMachinCoherency(taskCode, machineCode);
+		for (int i = 1; i <= 9; i++) {
+			TaskProductionUpdateMessage produpdate = new TaskProductionUpdateMessage(apsData);
+			produpdate.setMessageTimestamp(Timestamp.valueOf("2021-01-09 09:1" + i + ":59"));
+			produpdate.setTaskCode(taskCode);
+			produpdate.setMachineCode(machineCode);
+			response = broker.handleMessage(produpdate, apsData, null);
+			checkApsMessageResponse(response);
+			checkMachineState(machineCode, apsData, ReservableObjectAvailability.EXECUTING_WORK);
+			checkTaskState(taskCode, apsData, TaskStatus.EXECUTING_WORK);
+			checkChoosedMachinCoherency(taskCode, machineCode);
+		}
+		EndWorkMessage endWorkMessage = new EndWorkMessage(apsData);
+		endWorkMessage.setMessageTimestamp(Timestamp.valueOf("2021-01-09 09:10:59"));
+		endWorkMessage.setTaskCode(taskCode);
+		endWorkMessage.setMachineCode(machineCode);
+		response = broker.handleMessage(endWorkMessage, apsData, null);
+		checkApsMessageResponse(response);
+		checkMachineState(machineCode, apsData, ReservableObjectAvailability.AVAILABLE);
+		checkTaskState(taskCode, apsData, TaskStatus.EXECUTED);
+		checkChoosedMachinCoherency(taskCode, machineCode);
+
+		LOGGER.info("Executed in " + (System.currentTimeMillis() - timestamp) + " ms");
+
 	}
 }
