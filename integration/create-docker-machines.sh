66d8ddf9425dfac1c33aec89cefb0aaebeab69d6
openi40 <architectures@openi40.org>
2023-11-24 09:12:16 +0100

    docker images initial implementation for integration tests

---
+++ integration/create-docker-machines.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+rm *.jar
+export BOOTABLES=`find ../ -name *.bootable.jar -print`;
+for B in $BOOTABLES 
+do
+cp -r $B .
+done
+export LOCAL_SPRING_BOOTS=`ls *.bootable.jar`
+for F in $LOCAL_SPRING_BOOTS
+do
+export APPNAME=`echo $F|sed s/"com."// | sed s/".bootable.jar"// | sed s/'\.'/'\-'/g`
+echo "Managing $F with docker name $APPNAME";
+mkdir $APPNAME
+mv $F $APPNAME
+docker image rm $APPNAME
+docker build --build-arg JAVA_EXTRA_SECURITY_DIR=/app -t $APPNAME $APPNAME/
+done
