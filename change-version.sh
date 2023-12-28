34f006f96f4c158e5f0631b9c502f7bf4ee08da5
openi40 <architectures@openi40.org>
2023-12-28 18:47:47 +0100

    versioning raw bash script (to be improved)

--- change-version.sh
+++ change-version.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-echo Changing from version $1 to $2
-find . -name pom.xml | xargs sed -i s/"$1"/"$2/ 
+if [ -n "$1"  ] 
+then
+	echo "Old version: $1"
+else
+	echo "Required actual version as 1st parameter"
+	exit 1
+fi
+if [ -n "$2" ] 
+then
+	echo "New version: $2"
+else 
+	echo "Required New version as 2nd parameter"
+	exit 1
+fi
+echo "Changing from version $1 to $2"
+find . -name pom.xml | xargs sed -i s/"$1"/"$2"/ 
 find . -name pom.xml | xargs git stage 
 git commit -m"Changed version from $1 to $2"
 
