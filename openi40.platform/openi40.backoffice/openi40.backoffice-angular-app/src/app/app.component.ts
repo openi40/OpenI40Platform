f2d2793c1f5d2a0731ed8a8315a8b9b5e67a2ce8
openi40 <architectures@openi40.org>
2024-01-05 11:27:32 +0100

    backoffice UI, added menu items (WIP)

--- openi40.platform/openi40.backoffice/openi40.backoffice-angular-app/src/app/app.component.ts
+++ openi40.platform/openi40.backoffice/openi40.backoffice-angular-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { PrimeNGConfig } from 'primeng/api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(private primengConfig: PrimeNGConfig) {
 
   }
@@ -14,26 +14,60 @@ export class AppComponent implements OnInit{
     this.primengConfig.ripple = true;
   }
   items: MegaMenuItem[] = [{
-    label: 'production configuration',
+    label: 'companies organization',
+    icon: 'pi pi-fw pi-sitemap',
+    items: [
+      [
+        {
+          label: "companies/plants...", automationId: true, items: [
+            { label: "companies", automationId: true, url: "companies-search" },
+            { label: "plants", automationId: true, url: "plants-search" },
+            { label: "departments", automationId: true, url: "departments-search" },
+            { label: "warehouses", automationId: true, url: "warehouses-search" }
+          ]
+        }, {
+          label: "shop floor", automationId: true, items: [
+            { label: "workcenters", automationId: true, url: "workcenters-search" },
+            { label: "machines", automationId: true, url: "machines-search" },
+            { label: "resources group", automationId: true, url: "resource-groups-search" },
+            { label: "resources", automationId: true, url: "resources-search" }
+          ]
+        }]
+    ]
+  }, {
+    label: 'items & items production cfg',
     icon: 'pi pi-fw pi-cog',
     items: [
       [{ label: "products", automationId: true, items: [{ label: "products", automationId: true, url: "products-search" }] },
-      { label: "cycle/operations", automationId: true, items: [{ label: "cycles", automationId: true, url: "cycles-search" },{ label: "operations", automationId: true, url: "operations-search" }] }]
+      { label: "cycle/operations", automationId: true, items: [{ label: "cycles", automationId: true, url: "cycles-search" }, { label: "operations", automationId: true, url: "operations-search" }] }]
     ]
-  },{
+  }, {
     label: 'sales cycle',
-    icon: 'pi pi-fw pi-cog',
+    icon: 'pi pi-fw pi-dollar',
     items: [
-      [{ label: "orders", automationId: true, 
-          items: [{ label: "sales orders", automationId: true, url: "sales-orders" }] }]     
-        ]
-    },{
-      label: 'purchase cycle',
-      icon: 'pi pi-fw pi-cog',
-      items: [
-        [{ label: "orders", automationId: true, 
-            items: [{ label: "purchase orders", automationId: true, url: "purchase-orders" }] }]     
-          ]
-      }];
+      [{
+        label: "orders", automationId: true,
+        items: [{ label: "sales orders", automationId: true, url: "sales-orders" }]
+      }]
+    ]
+  }, {
+    label: 'purchase cycle',
+    icon: 'pi pi-fw pi-shopping-cart',
+    items: [
+      [{
+        label: "orders", automationId: true,
+        items: [{ label: "purchase orders", automationId: true, url: "purchase-orders" }]
+      }]
+    ]
+  }, {
+    label: 'production',
+    icon: 'pi pi-fw pi-wrench',
+    items: [
+      [{
+        label: "work orders/tasks", automationId: true,
+        items: [{ label: "work orders", automationId: true, url: "work-orders" }, { label: "production tasks", automationId: true, url: "production-tasks" }]
+      }]
+    ]
+  }];
   title = 'backoffice-app';
 }
