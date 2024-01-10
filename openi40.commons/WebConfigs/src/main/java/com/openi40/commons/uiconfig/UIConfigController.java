41a34ed37add67dc6e9513caa73989dd884db010
openi40 <architectures@openi40.org>
2024-01-10 08:58:52 +0100

    ui-angular-monorepo: backend ui configuration support (WIP)

--- openi40.commons/WebConfigs/src/main/java/com/openi40/commons/uiconfig/UIConfigController.java
+++ openi40.commons/WebConfigs/src/main/java/com/openi40/commons/uiconfig/UIConfigController.java
@@ -17,34 +17,38 @@ import com.openi40.commons.webconfigs.OpenI40WebConfig;
 @RestController
 @RequestMapping(path = "uiConfig/UIConfigController")
 public class UIConfigController {
-	static final String UI_CONFIG_FILE="classpath:/ui-config.json";
-	OpenI40WebConfig baseConfig=null;
-	static Logger LOGGER=LoggerFactory.getLogger(UIConfigController.class);
+	static final String UI_CONFIG_FILE = "ui-config.json";
+	OpenI40WebConfig baseConfig = null;
+	static Logger LOGGER = LoggerFactory.getLogger(UIConfigController.class);
+
 	public UIConfigController(@Autowired(required = false) OpenI40WebConfig config) {
-		baseConfig=config;
-		
+		baseConfig = config;
+
 	}
+
 	@GetMapping(produces = "application/json")
-	
+
 	public String get() throws IOException {
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("Begin get()");
 		}
-		String outValue="{}";
-		InputStream is=this.getClass().getResourceAsStream(UI_CONFIG_FILE);
-		if (baseConfig!=null && baseConfig.getAngularUi()!=null && baseConfig.getAngularUi() && is!=null) {
+		String outValue = "{}";
+		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+		InputStream is = classloader.getResourceAsStream(UI_CONFIG_FILE);
+		if (baseConfig != null && baseConfig.getAngularUi() != null && baseConfig.getAngularUi() && is != null) {
 			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("UI enabled and returned "+UI_CONFIG_FILE);
-				ByteArrayOutputStream bos=new ByteArrayOutputStream();
-				FileCopyUtils.copy(is, bos);
-				bos.flush();
-				outValue=bos.toString("UTF-8");
+				LOGGER.debug("UI enabled and returned " + UI_CONFIG_FILE);
 			}
-		}else {
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			FileCopyUtils.copy(is, bos);
+			bos.flush();
+			outValue = bos.toString("UTF-8");
+
+		} else {
 			throw new RuntimeException("Unavailable resource");
 		}
 		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("End get() returning "+outValue);
+			LOGGER.debug("End get() returning " + outValue);
 		}
 		return outValue;
 	}
